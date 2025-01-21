cd83e2ba2ec9cfbf7ee09bc05fb068a33c73affc
Skyler <Vloneurable@gmail.com>
2025-01-21 13:28:35 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,7 @@
 local Module = {}
 
 function Module.Load()
+    print("yes")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Vloneurable999/VadeScriptHub/refs/heads/main/Systems/LoadVade.lua"))().LoadVade()
 end
 
